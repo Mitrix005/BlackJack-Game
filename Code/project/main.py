4711abe0119b8707562e67e448ddb1bdc0cec204
Mitrix005 <108869424+Mitrix005@users.noreply.github.com>
2025-05-28 12:35:11 +0200

    Update main.py

--- Code/project/main.py
+++ Code/project/main.py
@@ -1,18 +1,18 @@
 import pygame
-#Ale gówno
+
 pygame.init()
-#Co my żyjemy pod kamieniem? Zrób 1280x720 przynajmniej
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#Po chuj ci running jak to i tak nie działa?
+
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-#Tak to każdy umie wychodzić!
+
 pygame.quit
-#cwelu
+
