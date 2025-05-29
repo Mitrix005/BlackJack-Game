ba6b9fc959058e36e00255d1aa64fa9e805c3ba8
KFC-Abuser <esobczyk@student.agh.edu.pl>
2025-05-29 03:08:33 +0200

    Aktualizacja klasy Button, dodanie nowych klas MusicManager ImageManager Sound, ustawienie menu gry

--- Code/main.py
+++ Code/main.py
@@ -14,21 +14,84 @@ class Button:
         self.hover_color = hover_color
         self.text_color = text_color
         self.font = font or pygame.font.SysFont(None, 36)
+        self.active = True
 
     def draw(self, screen):
+        if not self.active:
+            return False
         mouse = pygame.mouse.get_pos()
         is_hovered = self.rect.collidepoint(mouse)
         pygame.draw.rect(screen, self.hover_color if is_hovered else self.color, self.rect)
         text_surf = self.font.render(self.text, True, self.text_color)
         text_rect = text_surf.get_rect(centerx=self.rect.centerx, centery=self.rect.centery)
         screen.blit(text_surf, text_rect)
+        return True
 
     def handle_event(self, event):
+        if not self.active:
+            return False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if self.rect.collidepoint(pygame.mouse.get_pos()):
                     return True
         return False
+
+class Sound:
+    def __init__(self,path):
+        self.path = path
+        self.running = None
+        self.s = None #sound
+        self.type = type
+        self.load()
+
+    def load(self):
+        base_dir = os.path.dirname(__file__)
+        file = os.path.join(base_dir, self.path)
+        sound=pygame.mixer.Sound(file)
+        self.s=sound
+
+
+class MusicManager:
+    def __init__(self):
+        self.menu_music_path = self.load("../Audio/menu_theme.mp3")
+        self.game_music_path = self.load('../Audio/soundtrack.mp3')
+        self.state = None
+
+    def play_menu(self):
+        if self.state != "MENU":
+            pygame.mixer.music.load(self.menu_music_path)
+            pygame.mixer.music.play(-1)
+            self.state = "MENU"
+
+    def play_game(self):
+        if self.state != "GAME":
+            pygame.mixer.music.load(self.game_music_path)
+            pygame.mixer.music.play(-1)
+            self.state = "GAME"
+
+    def stop_music(self):
+        pygame.mixer.music.stop()
+        self.state = None
+
+    def load(self, path):
+        base_dir = os.path.dirname(__file__)
+        file = os.path.join(base_dir, path)
+        return file
+
+class ImageManager:
+    def __init__(self, path):
+        base_dir = os.path.dirname(__file__)
+        directory = os.path.join(base_dir, path)
+        self.base_dir = directory
+        self.cache = {}
+
+    def load(self, filename):
+        if filename not in self.cache:
+            path = os.path.join(self.base_dir, filename)
+            picture = pygame.image.load(path).convert()
+            self.cache[filename] = picture
+        return self.cache[filename]
+
 
 pygame.init()
 
@@ -38,51 +101,78 @@ SCREEN_HEIGHT = 832
 main_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("BlackJack-Game")
 
-button = Button(main_screen,300,100,200, 60,"Button", (40,40,40),(32,32,32))
+play_button = Button(main_screen,636,400,200, 60,"Play", (40,40,40),(32,32,32))
 
-gamble_button = Button(main_screen, 300, 200, 200, 60, "$Gamble$", (255,215,0), (255,190,0))
+gamble_button = Button(main_screen, 636, 500, 200, 60, "$ Gamble $", (255,215,0), (255,190,0))
+
+quit_button = Button(main_screen, 636, 600, 200, 60, "Quit", (40,40,40), (32,32,32))
+
+back_to_menu = Button(main_screen,1100, 650, 200, 60, "Back", (40,40,40),(32,32,32))
 
 #Zmienne Gry
+music_manager = MusicManager()
+image_manager = ImageManager('../Graphics')
 
-base_dir = os.path.dirname(__file__)
+jackpot_sound = Sound('../Audio/Jackpot.wav')
 
-main_menu_audio_path = os.path.join(base_dir,'../Audio/menu_theme.mp3')
+main_menu_background=image_manager.load("menu_background.jpg")
+logo=image_manager.load("logo.jpg")
+table=image_manager.load("stol.jpg")
 
-jackpot_audio_path = os.path.join(base_dir,'../Audio/Jackpot.wav')
-
-main_menu_background_path = os.path.join(base_dir,'../Graphics/menu_background.jpg')
-
-logo_path = os.path.join(base_dir,'../Graphics/logo.jpg')
-
-main_menu_audio = pygame.mixer.Sound(main_menu_audio_path)
-main_menu_audio.set_volume(0.5)
-
-jackpot_sound = pygame.mixer.Sound(jackpot_audio_path)
-jackpot_sound.set_volume(0.5)
-
-main_menu_background = pygame.image.load(main_menu_background_path)
-
-logo = pygame.image.load(logo_path)
+pygame.mixer.music.set_volume(0.5)
+jackpot_sound.s.set_volume(0.5)
 
 pygame.display.set_icon(logo)
 
+state="MENU"
+
+menu_music_playing=False
 running = True
-main_menu_audio.play()
 while running:
-    #main_screen.fill((105, 105, 105))
-    main_screen.blit(main_menu_background, (0, 0))
-
-    button.draw(main_screen)
-
-    gamble_button.draw(main_screen)
-
 
     for event in pygame.event.get():
-        if button.handle_event(event):
-            print("siggma")
-        if gamble_button.handle_event(event):
-            jackpot_sound.play()
         if event.type == pygame.QUIT:
             running = False
+        if state=="MENU":
+            if play_button.handle_event(event):
+                state="GAME"
+            if gamble_button.handle_event(event):
+                jackpot_sound.s.play()
+            if quit_button.handle_event(event):
+                running = False
+        if state=="GAME":
+            back_to_menu.draw(main_screen)
+            if back_to_menu.handle_event(event):
+                state="MENU"
+
+
+
+    if state == "MENU":
+        if music_manager.state!="MENU":
+            music_manager.play_menu()
+        back_to_menu.active = False
+        play_button.active = True
+        gamble_button.active = True
+        quit_button.active = True
+
+        main_screen.blit(main_menu_background, (0, 0))
+
+        play_button.draw(main_screen)
+
+        gamble_button.draw(main_screen)
+
+        quit_button.draw(main_screen)
+    if state == "GAME":
+        play_button.active = False
+        gamble_button.active = False
+        quit_button.active = False
+        back_to_menu.active = True
+        if music_manager.state!="GAME":
+            music_manager.play_game()
+
+        main_screen.blit(table, (0, 0))
+        back_to_menu.draw(main_screen)
+
+
     pygame.display.flip()
 pygame.quit()
